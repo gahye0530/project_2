5d47fe004d6272c92d33bf5a7b6c87b478b0022b
gahye0530 <40849083+gahye0530@users.noreply.github.com>
2021-12-24 10:48:43 +0900

    Update area_search.py

--- area_search.py
+++ area_search.py
@@ -3,6 +3,9 @@ import streamlit as st
 import pandas as pd
 from draw_map import run_draw_map
 import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import altair as alt
 
 
 def run_area_search() :
@@ -20,5 +23,12 @@ def run_area_search() :
 
     # streamlit의 bar_chart를 이용하면 정렬이 안되고
     # dataframe의 plot을 이용하면 한글이 안먹히고..
-    st.bar_chart(chart_df)
+    # metaplotlib.pyplot bar차트를 이용해도 한글이 안먹히고.. 정홍근님이 알려준 방법도 안먹히고..
     
+    st.bar_chart(chart_df)
+
+    # altair는 value_counts한 데이터프레임 chart_df가 적합하지 않은것같고..
+    # st.write(alt.Chart(chart_df).mark_bar().encode(
+    #     x = x,
+    #     y = y
+    # ))
