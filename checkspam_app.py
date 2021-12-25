b2bbb4f9aab2ec04c9c1d20e805a5075bfa622f6
gahye0530 <40849083+gahye0530@users.noreply.github.com>
2021-12-25 23:30:17 +0900

    mysql_update

--- checkspam_app.py
+++ checkspam_app.py
@@ -5,51 +5,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from mysql.connector import errors
+import wordcloud
 
 from mysql_connect import get_connection
-
-spam_df = pd.read_csv('data/emails.csv')
-
-def run_checkspam(vectorizer, classifier, my_stopwords) :
-    st.subheader('Please enter your message')
-    text = [st.text_area('','',height = 100, placeholder='Type here...')]
-
-    if (st.button('확인')) & (text != '') :
-        X_sample = vectorizer.transform(text)
-        X_sample = X_sample.toarray()
-        y_pred_sample = classifier.predict(X_sample)
-        print(y_pred_sample[0].dtype)
-        if y_pred_sample[0]==1 :
-            st.write('Similar to messages previously identified as spam.')
-            word_cloud(my_stopwords)
-            result_send(text, 1)
-        else :
-            st.write('Not Spam.')
-            word_cloud(my_stopwords)
-            result_send(text, 0)
-
-def result_send(text, spam) :
-    st.subheader('분석결과를 서버로 보내주시겠습니까?')
-    if st.button('Yes') :
-        try :
-            print('yyyyyeeeeeeesssssssss')
-            connection = get_connection()
-            query = '''insert into new_text(text, spam) values('%s',%s);'''
-            record = (text, spam)
-            cursor = connection.cursor()
-            cursor.execute(cursor, record)
-            connection.commit
-        except errors as e :
-            print('Error', e)
-        finally :
-            if connection.is_connected() :
-                cursor.close()
-                connection.close()
-                st.write('Success')
-
-
-
-
 
 # 워드클라우드
 # pip install wordcloud
@@ -69,7 +27,41 @@ def word_cloud(my_stopwords) :
     st.pyplot(fig)
 
 
+spam_df = pd.read_csv('data/emails.csv')
 
+def run_checkspam(vectorizer, classifier, my_stopwords) :
+    st.subheader('Please enter your message')
+    # 학습을 위해 리스트 형태로 가져온다. 
+    text = [st.text_area('','',height = 100, placeholder='Type here...')]
 
+    if (st.button('확인')) & (text != '') :
+        X_sample = vectorizer.transform(text)
+        X_sample = X_sample.toarray()
+        y_pred_sample = classifier.predict(X_sample)
+        
+        if y_pred_sample[0]==1 :
+            st.write('Similar to messages previously identified as spam.')
+        else :
+            st.write('Not Spam.')
 
+        word_cloud(my_stopwords)
+        # 리스트 형태로 가지고 온 text를 문자열 형태로 result_send함수를 호출한다. 
+        result_send(text[0], 1)
 
+def result_send(text, spam) :
+    # st.subheader('분석결과를 서버로 보내주시겠습니까?')
+    # if st.button('Yes') :
+    try :
+        connection = get_connection()
+        query = '''insert into new_text(text, spam) values(%s,%s);'''
+        record = (text, spam)
+        cursor = connection.cursor()
+        cursor.execute(query, record)
+        connection.commit
+    except errors as e :
+        print('Error', e)
+    finally :
+        if connection.is_connected() :
+            cursor.close()
+            connection.close()
+            st.write('Success')
