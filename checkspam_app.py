f98176943b18164119d7808cc7e0aba318790b0f
gahye0530 <40849083+gahye0530@users.noreply.github.com>
2021-12-27 01:51:52 +0900

    final

--- checkspam_app.py
+++ checkspam_app.py
@@ -5,6 +5,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from mysql.connector import errors
+from wordcloud.wordcloud import STOPWORDS
 
 from mysql_connect import get_connection
 
@@ -15,10 +16,14 @@ from PIL import Image
 
 spam_df = pd.read_csv('data/emails.csv')
 
-def word_cloud(my_stopwords) :
-    words_as_one_string = ''.join(spam_df['text'].tolist())
+def word_cloud() :
+    my_stopwords = STOPWORDS
+    words_as_one_string = ''.join(spam_df.loc[spam_df['spam']==1,'text'].tolist())
     img = Image.open('data/spam_img.JPG')
     img_mask = np.array(img)
+    my_stopwords.add('Subject')
+    my_stopwords.add('us')
+    my_stopwords.add('one')
     wc = WordCloud(background_color='white', mask = img_mask, stopwords = my_stopwords)
     wc.generate(words_as_one_string)
 
@@ -27,7 +32,7 @@ def word_cloud(my_stopwords) :
     plt.axis('off')
     st.pyplot(fig)
 spam=0
-def run_checkspam(vectorizer, classifier, my_stopwords) :
+def run_checkspam(vectorizer, classifier) :
     st.subheader('Please enter your message')
     # 학습을 위해 리스트 형태로 가져온다. 
     text = [st.text_area('','',height = 100, placeholder='Type here in english...')]
@@ -49,10 +54,10 @@ def run_checkspam(vectorizer, classifier, my_stopwords) :
     st.write('')
     # 리스트 형태로 가지고 온 text를 문자열 형태로 result_send함수를 호출한다. 
     # 텍스트를 apply하거나 버튼하나를 눌러도 새로고침돼서 spam결과를 보내기 어렵다. 
-    result_send(text[0], spam, my_stopwords)
+    result_send(text[0], spam)
     
 
-def result_send(text, spam, my_stopwords) :
+def result_send(text, spam) :
     st.write('Transmission of analysis results?')
     if st.button('Yes') :
         try :
@@ -69,4 +74,4 @@ def result_send(text, spam, my_stopwords) :
                 cursor.close()
                 connection.close()
                 # st.write('Success')
-                word_cloud(my_stopwords)
+                word_cloud()
