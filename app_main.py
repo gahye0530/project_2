3a44caa9d747998e9883c6959d1206fca0f789f1
gahye0530 <40849083+gahye0530@users.noreply.github.com>
2021-12-27 12:41:39 +0900

    12271241

--- app_main.py
+++ app_main.py
@@ -1,6 +1,3 @@
-import streamlit as st
-import pandas as pd
-from attract_tourism import run_at
 from checkspam_app import run_checkspam
 import joblib
 import string
@@ -19,13 +16,8 @@ def main() :
     # 피클파일로 가지고 오면 정확도 산출이 어렵다. 
     vectorizer = joblib.load('data/vectorizer.pkl')
     classifier = joblib.load('data/classifier1.pkl')
-    choice = st.sidebar.selectbox('검색선택',['국내 관광 명소', 'SPAM 문자 확인'])
 
-    if choice == '국내 관광 명소' :
-        run_at()
-        
-    elif choice == 'SPAM 문자 확인' :
-        run_checkspam(vectorizer, classifier) 
+    run_checkspam(vectorizer, classifier) 
         
     
 if __name__ == '__main__' : main()
